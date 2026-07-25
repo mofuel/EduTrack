504061ddf1dcda46aa4c9d12353453a5d6f33ccf
Miguel Monzon <miguel.monzon.lucio@gmail.com>
2026-07-24 22:09:47 -0500

    Fix DTO PagoDTO

--- src/main/java/com/EduTrack/domain/dto/PagoDTO.java
+++ src/main/java/com/EduTrack/domain/dto/PagoDTO.java
@@ -1,36 +1,29 @@
 package com.EduTrack.domain.dto;
 
+import com.EduTrack.persistence.entity.EstadoPago;
 import java.time.LocalDateTime;
 
 public class PagoDTO {
     private Long id;
-    private String usuarioId;
+    private Long usuarioId;
     private Long cursoId;
     private String metodoPago;
     private String referenciaPago;
+    private EstadoPago estado;
     private LocalDateTime fechaPago;
 
-    public Long getId() {return id;}
-
-    public void setId(Long id) {this.id = id;}
-
-    public String getUsuarioId() {return usuarioId;}
-
-    public void setUsuarioId(String usuarioId) {this.usuarioId = usuarioId;}
-
-    public Long getCursoId() {return cursoId;}
-
-    public void setCursoId(Long cursoId) {this.cursoId = cursoId;}
-
-    public String getMetodoPago() {return metodoPago;}
-
-    public void setMetodoPago(String metodoPago) {this.metodoPago = metodoPago;}
-
-    public String getReferenciaPago() {return referenciaPago;}
-
-    public void setReferenciaPago(String referenciaPago) {this.referenciaPago = referenciaPago;}
-
-    public LocalDateTime getFechaPago() {return fechaPago;}
-
-    public void setFechaPago(LocalDateTime fechaPago) {this.fechaPago = fechaPago;}
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public Long getUsuarioId() { return usuarioId; }
+    public void setUsuarioId(Long usuarioId) { this.usuarioId = usuarioId; }
+    public Long getCursoId() { return cursoId; }
+    public void setCursoId(Long cursoId) { this.cursoId = cursoId; }
+    public String getMetodoPago() { return metodoPago; }
+    public void setMetodoPago(String metodoPago) { this.metodoPago = metodoPago; }
+    public String getReferenciaPago() { return referenciaPago; }
+    public void setReferenciaPago(String referenciaPago) { this.referenciaPago = referenciaPago; }
+    public EstadoPago getEstado() { return estado; }
+    public void setEstado(EstadoPago estado) { this.estado = estado; }
+    public LocalDateTime getFechaPago() { return fechaPago; }
+    public void setFechaPago(LocalDateTime fechaPago) { this.fechaPago = fechaPago; }
 }
